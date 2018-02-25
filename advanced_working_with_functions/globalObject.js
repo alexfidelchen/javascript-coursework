7ace2a71ec04326f59846948dc1488fd0a06b52d
Alex Chen <alexanderfschen@gmail.com>
2018-02-25 00:52:28 -0800

    added scripts

---
+++ advanced_working_with_functions/globalObject.js
@@ -0,0 +1,12 @@
+// GLOBAL OBJECT 
+
+/* The global object is the object we start out with when we write
+	a script. The object is already initialized and we can use it
+	at the start of the script. The cool thing about the global
+	object is that we don't have to reference the object directly 
+	when we call its methods. 
+*/
+
+/* So again, the object we start object with in the browser is 
+	window. 
+*/
